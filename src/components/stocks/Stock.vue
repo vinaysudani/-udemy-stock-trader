6d1136470c4b147422dc0260bc30abc477818add
Vinay Sudani <vinaysudani9@gmail.com>
2020-08-14 10:16:03 +0530

    Added stocks components

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,3 +1,49 @@
 <template>
-    <h1>Stocks - stock component</h1>
+    <div class="col-sm-6 col-md-4 my-1">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title text-success">
+                    {{ stock.name }}
+                    <small>(Price: {{ stock.price }})</small>
+                </h5>
+                <div class="float-left">
+                    <input
+                        type="number"
+                        class="form-control"
+                        placeholder="Quantity"
+                        v-model="quantity"
+                        >
+                </div>
+                <div class="float-right">
+                    <button
+                        class="btn btn-success"
+                        @click="buyStock"
+                        :disabled="quantity <= 0"
+                        >Buy</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+
+<script>
+export default {
+    props: ['stock'],
+    data() {
+        return {
+            quantity: 0
+        }
+    },
+    methods: {
+        buyStock() {
+            const order = {
+                stockId: this.stock.id,
+                stockPrice: this.stock.price,
+                quantity: this.quantity
+            }
+            console.log(order)
+            this.quantity = 0;
+        }
+    }
+}
+</script>
